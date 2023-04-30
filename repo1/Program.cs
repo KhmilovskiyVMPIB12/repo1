fe660592e0a09704481be04df2cb43ad3593cc6b
Хмиловский Владислав Михайлович ПИБ12 <Ufo.horek@gmail.com>
2023-04-30 14:45:28 +0700

    изменил код расчёта

--- repo1/Program.cs
+++ repo1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
 
 class Program
 {
@@ -8,7 +9,7 @@ class Program
         Console.WriteLine("Введите год:");
         int year = int.Parse(Console.ReadLine());
 
-        if (year % 4 == 0)
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
         {
             Console.WriteLine("{0} - високосный год", year);
         }
