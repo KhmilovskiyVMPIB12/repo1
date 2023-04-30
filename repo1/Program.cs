4d8bdac2bd5be617499d4674a24a9a400f8097aa
Хмиловский Владислав Михайлович ПИБ12 <Ufo.horek@gmail.com>
2023-04-30 14:31:51 +0700

    1 версия программы

---
+++ repo1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите год:");
+        int year = int.Parse(Console.ReadLine());
+
+        if (year % 4 == 0)
+        {
+            Console.WriteLine("{0} - високосный год", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} - не високосный год", year);
+        }
+    }
+}
